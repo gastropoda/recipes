42b4903e9984a8259cf4f394f09571b526cf9735
Artem Baguinski <abaguinski@depraktijkindex.nl>
2015-06-07 07:50:07 +0200

    attempt to reload cookbooks if updated

--- cookbooks/gastropoda_recipes/recipes/default.rb
+++ cookbooks/gastropoda_recipes/recipes/default.rb
@@ -12,4 +12,12 @@ git "/home/artm/.chef" do
   user "artm"
   group "artm"
   action :sync
+  notifies :create, "ruby_block[reload_cookbooks]", :immediately
+end
+
+ruby_block "reload_cookbooks" do
+  block do
+    Chef::Config.from_file("#{node["chef_client"]["conf_dir"]}/client.rb")
+  end
+  action :nothing
 end
