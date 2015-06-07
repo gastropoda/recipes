a1086b04a29796df79c6f4f64317b31245f295b4
Artem Baguinski <abaguinski@depraktijkindex.nl>
2015-06-07 08:54:03 +0200

    manage /etc/chef/client.rb

--- cookbooks/gastropoda_recipes/recipes/default.rb
+++ cookbooks/gastropoda_recipes/recipes/default.rb
@@ -6,6 +6,14 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+template "/etc/chef/client.rb" do
+  source "client.rb.erb"
+  owner "root"
+  group "root"
+  variables principal_user: "artm"
+end
+
 git "/home/artm/.chef" do
   repository "git@github.com:gastropoda/recipes.git"
   revision "master"
@@ -17,6 +25,8 @@ end
 
 ruby_block "chef-client" do
   block do
+    # This will spawn another chef-client instance that will wait for this one to finish
+    # and then run with new cookbooks
     spawn "chef-client"
   end
   action :nothing
