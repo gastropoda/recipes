16464dd6dab7aa53b576faf6707a0d8ff12eea8d
Artem Baguinski <abaguinski@depraktijkindex.nl>
2015-06-08 11:45:27 +0200

    virtual box recipe

---
+++ cookbooks/essential_packages/recipes/virtual_box.rb
@@ -0,0 +1,13 @@
+require "uri"
+
+url = "http://download.virtualbox.org/virtualbox/4.3.28/virtualbox-4.3_4.3.28-100309~Ubuntu~raring_amd64.deb"
+base_name = File.basename(URI.parse(url).path)
+cached_file = "#{Chef::Config[:file_cache_path]}/#{base_name}"
+
+remote_file cached_file do
+  source url
+end
+
+dpkg_package cached_file do
+  action :install
+end
