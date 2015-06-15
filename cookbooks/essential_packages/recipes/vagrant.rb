7d4701c564d12b07e6d12eab97752b7f51ba9616
Artem Baguinski <abaguinski@depraktijkindex.nl>
2015-06-15 11:05:03 +0200

    downgrade vagrant to 1.7.1 due to issue 5199
    https://github.com/mitchellh/vagrant/issues/5199

--- cookbooks/essential_packages/recipes/vagrant.rb
+++ cookbooks/essential_packages/recipes/vagrant.rb
@@ -1,1 +1,1 @@
-essential_packages_remote_deb "https://dl.bintray.com/mitchellh/vagrant/vagrant_1.7.2_x86_64.deb"
+essential_packages_remote_deb "https://dl.bintray.com/mitchellh/vagrant/vagrant_1.7.1_x86_64.deb"
