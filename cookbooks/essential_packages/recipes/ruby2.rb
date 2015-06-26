c6a1254c7c7b51a74f067efbbaab0e5b982e7658
Artem Baguinski <abaguinski@depraktijkindex.nl>
2015-06-26 10:40:50 +0200

    ruby2 recipe

---
+++ cookbooks/essential_packages/recipes/ruby2.rb
@@ -0,0 +1,20 @@
+package "ruby2.0"
+
+# See https://bugs.launchpad.net/ubuntu/+source/ruby2.0/+bug/1310292
+# and https://bugs.launchpad.net/ubuntu/+source/ruby2.0/+bug/1310292/comments/25
+bash "Make ruby2 default" do
+  code <<-__
+    dpkg-divert --add --rename --divert /usr/bin/ruby.divert /usr/bin/ruby
+    update-alternatives --install /usr/bin/ruby ruby /usr/bin/ruby2.0 1
+  __
+  creates "/usr/bin/ruby.divert"
+end
+
+bash "Make gem2 default" do
+  code <<-__
+    dpkg-divert --add --rename --divert /usr/bin/gem.divert /usr/bin/gem
+    update-alternatives --install /usr/bin/gem gem /usr/bin/gem2.0 1
+  __
+  creates "/usr/bin/gem.divert"
+end
+
